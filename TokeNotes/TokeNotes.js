d4f90e91f464204c7e15f746855bd1e4d1aa01b3
chrisstufflebeam <chrisstufflebeam@gmail.com>
2015-03-26 23:46:25 -0700

    Fixed underflow for smaller tokens.

--- TokeNotes/TokeNotes.js
+++ TokeNotes/TokeNotes.js
@@ -12,17 +12,20 @@ gmn.SetPopupData = function(data, token, bar, size) {
     // What follows is a hack to enable the use of a "bar" as a presentation-layer. 
     // this permits us to display special qualities to the GM upon clicking on a token, without having to open up gmnotes/etc.
     var fmt = "";
+    if (size == 0)
+        size++;
     var seperator = "              ".repeat(size+1); // Fill with unicode non-breaking spaces, adjusted for token size
     if (Object.prototype.toString.call(data) === '[object Array]' ) {
-	for(var i=0;i<data.length;i++) {
+    for(var i=0;i<data.length;i++) {
             fmt = fmt.concat(seperator, gmn.SanitizePopupData(data[i]), " ");
-	}
+    }
     }
     else {
-	fmt = seperator.concat(gmn.SanitizePopupData(data));
+    fmt = seperator.concat(gmn.SanitizePopupData(data));
     }
-    if (fmt.length <= 70)
-	fmt = fmt.concat(" ", seperator.repeat(Math.ceil((70-fmt.length)/14)));
+    if (fmt.length <= 90)
+    fmt = fmt.concat(seperator.repeat(Math.ceil((90-fmt.length)/14)));
+    //log("Setting fmt to "+fmt);
     token.set(bar, fmt);
     // End presentation layer hack.
 };
@@ -30,7 +33,7 @@ gmn.SetPopupData = function(data, token, bar, size) {
 on("change:graphic:gmnotes", function(obj, prev) {
     var data = obj.get("gmnotes");
     if (data.length > 0)
-	gmn.SetPopupData(decodeURI(data).split("<br>"), obj, gmn.gmNotesBar, Math.ceil(obj.get("width")/70) - 1);
+	gmn.SetPopupData(decodeURIComponent(data).split("<br>"), obj, gmn.gmNotesBar, Math.ceil(obj.get("width")/70) - 1);
     else
 	obj.set(gmNotesBar, "");
 });
