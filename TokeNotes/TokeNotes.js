7eb8b499fc5562ec1314316ef426be8d7f17cb52
chrisstufflebeam <chrisstufflebeam@gmail.com>
2015-03-27 18:03:27 -0700

    Added line-wrapping functionality.

--- TokeNotes/TokeNotes.js
+++ TokeNotes/TokeNotes.js
@@ -1,6 +1,7 @@
 // GM notes popup script
 var gmn = gmn || {};
 gmn.gmNotesBar = "bar2_value";
+gmn.lineLength = 60;
 
 gmn.SanitizePopupData = function(x) {
     x = x.replace(/ /g, " "); // Replace space character with unicode non-breaking character
@@ -10,23 +11,53 @@ gmn.SanitizePopupData = function(x) {
     return x;
 };
 
+gmn.LineWrap = function(seperator, sanitizedData) {
+    var lineWrapReg = new RegExp(".{1,"+gmn.lineLength+"}","g");
+    var output = "";
+    var rr = sanitizedData.match(lineWrapReg);
+    var wrapMode = false;
+    if (rr && sanitizedData.length>gmn.lineLength) {
+	log("Wrapping: " + sanitizedData);
+	output = " "+seperator;
+	for(var j=0;j<rr.length;j++) {
+	    // Find last "space" (Unicode character, since data has already been sanitized).
+	    var tr = /(.+) (.+?)$/.exec(rr[j]);
+	    if (tr) {
+		output = output.concat(tr[1], " ", seperator, tr[2]);
+		wrapMode = true;
+	    }
+	    else {
+		
+		// No spaces - Just append entire string.
+		output = output.concat((wrapMode)?" "+seperator:"", rr[j], " ", seperator);
+		wrapMode = false;
+	    }
+	}
+    }
+    else {
+	// less than lineLength characters long
+	output = " ".concat(seperator, sanitizedData);
+    }
+    return output;
+}
+
 gmn.SetPopupData = function(data, token, bar, size) {
     // What follows is a hack to enable the use of a "bar" as a presentation-layer. 
     // this permits us to display special qualities to the GM upon clicking on a token, without having to open up gmnotes/etc.
-    var fmt = "";
     if (size == 0)
         size++;
     var seperator = "              ".repeat(size+1); // Fill with unicode non-breaking spaces, adjusted for token size
+    var fmt = "";
     if (Object.prototype.toString.call(data) === '[object Array]' ) {
-    for(var i=0;i<data.length;i++) {
-            fmt = fmt.concat(seperator, gmn.SanitizePopupData(data[i]), " ");
-    }
+	for(var i=0;i<data.length;i++) {
+            fmt += gmn.LineWrap(seperator, gmn.SanitizePopupData(data[i]));
+	}
     }
     else {
-    fmt = seperator.concat(gmn.SanitizePopupData(data));
+	fmt = seperator.concat(gmn.LineWrap(seperator, gmn.SanitizePopupData(data)));
     }
     if (fmt.length <= 90)
-    fmt = fmt.concat(seperator.repeat(Math.ceil((90-fmt.length)/14)));
+	fmt = fmt.concat(seperator.repeat(Math.ceil((90-fmt.length)/14)));
     //log("Setting fmt to "+fmt);
     token.set(bar, fmt);
     // End presentation layer hack.
@@ -36,7 +67,7 @@ gmn.SetPopupData = function(data, token, bar, size) {
 on("change:graphic:gmnotes", function(obj, prev) {
     var data = obj.get("gmnotes");
     if (data.length > 0)
-    gmn.SetPopupData(unescape(data).split("<br>"), obj, gmn.gmNotesBar, Math.ceil(obj.get("width")/70) - 1);
+	gmn.SetPopupData(unescape(data).split("<br>"), obj, gmn.gmNotesBar, Math.ceil(obj.get("width")/70) - 1);
     else
 	obj.set(gmNotesBar, "");
 });
